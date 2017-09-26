640bdf790c9c795315d326d2da0d2a31af91a5e3
SDiego <SDiego@SDiego-PC>
2017-09-26 11:12:15 -0600

    puse pedo en la calse usrfram2

--- src/COMIDA_RED/userFrame2.java
+++ src/COMIDA_RED/userFrame2.java
@@ -13,7 +13,7 @@ public class userFrame2 extends javax.swing.JFrame {
     public static Menus menuuser2;
     public static Usuario cuenta2;
     public static Usuario cuenta3;
-    int pedo;int pedo2;
+    int pedo;
     /**
      * Creates new form userFrame2
      */
